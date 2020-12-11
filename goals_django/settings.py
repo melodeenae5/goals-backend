893850b4ada10a839f569f798f1a4d73bc48faad
melodeenae5 <danaekirby5@gmail.com>
2020-12-11 15:42:21 -0600

    changed settings

--- goals_django/settings.py
+++ goals_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v8otggdjq$(w%s99_k-c=i*u!b(q*a@3pfqr_*d-u21=ooctaa'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
